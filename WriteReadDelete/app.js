5ae9005e0bf8fbd08d031f069c4f71e443416598
Quinn Cooper <qcoop918@mtroyal.ca>
2023-04-19 16:48:36 -0600

    trying to fix leak

--- WriteReadDelete/app.js
+++ WriteReadDelete/app.js
@@ -14,31 +14,28 @@ app.get("/writeReadDelete", function (req, res) {
     let now = new Date()
     var err_flag = false
     for (let i = 0; i < 10; i++) {
-      let write = fs.writeFileSync(path.join(__dirname, now.getTime()+'_'+i+'.txt'), now.getTime()+'_'+i, err => {
+      let write = fs.writeFile(path.join(__dirname, now.getTime()+'_'+i+'.txt'), now.getTime()+'_'+i, err => {
         if (err) {
           err_flag = true
           console.log(err)
         }
-      })
-      let read = fs.readFileSync(path.join(__dirname, now.getTime()+'_'+i+'.txt'), 'utf8', (err, data) =>{
+      
+      let read = fs.readFile(path.join(__dirname, now.getTime()+'_'+i+'.txt'), 'utf8', (err, data) =>{
         if (err) {
           err_flag = true
           console.log(err)
         }
         console.log(data)
-      })
-      let del = fs.unlinkSync(path.join(__dirname, now.getTime()+'_'+i+'.txt'), (err) => {
+     
+      let del = fs.unlink(path.join(__dirname, now.getTime()+'_'+i+'.txt'), (err) => {
         if (err){
           err_flag = true
           console.log(err)
         }
-	write
-	read
-	del
 	write.destroy()
 	read.destroy()
 	del.destroy()
-      });
+      })})})
     }
     if(err_flag){
       res.statusCode = 500
