d6489d188d079dd1a65235debcb0c127627e8ff7
qcoop918 <47125657+qcoop918@users.noreply.github.com>
2023-04-24 13:02:57 -0600

    Update app.js

--- WriteReadDelete/app.js
+++ WriteReadDelete/app.js
@@ -14,14 +14,14 @@ app.get("/writeReadDelete", function (req, res) {
     let now = new Date()
     let readArray = []
     for (let i = 0; i < 10; i++) {
-      fs.writeFileSync(path.join(__dirname, now.getTime()+'_'+i+'.txt'), now.getTime()+'_'+i)
-      let read = fs.readFileSync(path.join(__dirname, now.getTime()+'_'+i+'.txt'), 'utf8')
-      fs.unlinkSync(path.join(__dirname, now.getTime()+'_'+i+'.txt')})
-	readArray.push(read)
+      fs.writeFileSync(path.join(__dirname, now.getTime() + '_' + i + '.txt'), now.getTime() + '_' + i)
+      let read = fs.readFileSync(path.join(__dirname, now.getTime() + '_' + i + '.txt'), 'utf8')
+      fs.unlinkSync(path.join(__dirname, now.getTime() + '_' + i + '.txt'))
+      readArray.push(read)
     }
-    
-    res.statuscode = 200
-    res.send("Jobs done " +  readArray)
+
+res.statuscode = 200
+res.send("Jobs done " + readArray)
 
   }
 });
