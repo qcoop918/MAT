ee77f95c310be6f4d9dcc296afb7dfdadeb94f68
qcoop918 <47125657+qcoop918@users.noreply.github.com>
2023-06-01 16:25:52 -0600

    Update app.js

--- Bubble/app.js
+++ Bubble/app.js
@@ -10,7 +10,7 @@ app.get("/bubble", function (req, res) {
   else {
     // This is our unsorted array
     var arr = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 1000; i++) {
       arr.push(random())
     }
     arr = bblSort(arr)
